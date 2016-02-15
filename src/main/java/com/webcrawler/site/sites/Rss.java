986fbce3e2589f156deb7894b8a4a4211b9c49d1
Robin <Robin.edbom@gmail.com>
2016-02-15 12:32:24 +0100

    Cleaned upp the Rss class from some old stuff

--- src/main/java/com/webcrawler/site/sites/Rss.java
+++ src/main/java/com/webcrawler/site/sites/Rss.java
@@ -6,19 +6,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
-import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
-
 public class Rss extends Site {
-
-    private HashMap<String, String> hashMap = null;
 
     public Rss(String url) {
         super(url);
     }
-
-    //https://www.torrentday.com/torrents/rss?download;1;2;3;5;7;11;13;14;21;22;24;25;26;31;32;33;44;46;u=2191010;tp=38a72acbc991348fd6e82c80ca12625d
-
 
     /**
      * Searches for the thing String casted as a param and returns a object if match is found.

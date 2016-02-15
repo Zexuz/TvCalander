e8de72cec41b8ebf16738d04cb5a38ffb9d817ee
Robin <Robin.edbom@gmail.com>
2016-02-15 16:21:27 +0100

    Added data type torrents

--- src/main/java/com/webcrawler/MainThread.java
+++ src/main/java/com/webcrawler/MainThread.java
@@ -1,8 +1,14 @@
 package com.webcrawler;
 
+import com.webcrawler.misc.Common;
 import com.webcrawler.site.SiteManager;
 import com.webcrawler.site.sites.Rss;
+import com.webcrawler.site.sites.TPB;
 import com.webcrawler.task.TaskManager;
+import com.webcrawler.torrent.Torrent;
+import com.webcrawler.torrent.TorrentManager;
+
+import java.util.ArrayList;
 
 public class MainThread implements Runnable {
 
@@ -12,23 +18,26 @@ public class MainThread implements Runnable {
 
     public SiteManager siteManager;
     public TaskManager taskManager;
+    public TorrentManager torrentManager;
 
     private MainThread() {
         siteManager = new SiteManager();
         taskManager = new TaskManager();
+        torrentManager = new TorrentManager();
+
         start();
     }
 
     @Override
     public void run() {
-        Rss rss = new Rss("https://www.torrentday.com");
-        rss.setPath("/torrents/rss?download;1;2;3;5;7;11;13;14;21;22;24;25;26;31;32;33;44;46;u=2191010;tp=38a72acbc991348fd6e82c80ca12625d");
-
-        siteManager.addSite("rss",rss);
+        // Rss rss = new Rss("https://www.torrentday.com");
+        // rss.setPath("/torrents/rss?download;1;2;3;5;7;11;13;14;21;22;24;25;26;31;32;33;44;46;u=2191010;tp=38a72acbc991348fd6e82c80ca12625d");
 
 
-        System.out.println(siteManager.getSite("rss").getPath());
+        TPB tpb = new TPB();
+        tpb.setPath("/recent/0");
 
+        System.out.println(Common.toJson(torrentManager.getTorrentTPB(tpb)));
 /*
 
 
@@ -48,7 +57,6 @@ public class MainThread implements Runnable {
         }
 
 
-        stop();
     }
 
 
---
+++ src/main/java/com/webcrawler/torrent/Torrent.java
@@ -0,0 +1,44 @@
+package com.webcrawler.torrent;
+
+import java.util.Date;
+
+public class Torrent {
+
+    private String title;
+    private String siteLink;
+    private String torrentLink;
+    private String description;
+    private Date pubDate;
+
+    public Torrent(String title, String siteLink, String torrentLink, String description, Date pubDate) {
+        this.title = title;
+        this.siteLink = siteLink;
+        this.torrentLink = torrentLink;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
+
+    public Torrent(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSiteLink() {
+        return siteLink;
+    }
+
+    public String getTorrentLink() {
+        return torrentLink;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getPubDate() {
+        return pubDate;
+    }
+}

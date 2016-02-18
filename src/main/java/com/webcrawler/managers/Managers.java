44b994915d038f6fe8f404c25c8136dded9d8c55
Robin.Edbom@gmail.com <Robin.Edbom@gmail.com>
2016-02-18 20:11:43 +0100

    Made some cleaning in the code.

--- src/main/java/com/webcrawler/managers/Managers.java
+++ src/main/java/com/webcrawler/managers/Managers.java
@@ -8,72 +8,64 @@ import java.util.ArrayList;
 
 public class Managers {
 
-    private TorrentManager torrentManager;
     private SiteManager siteManager;
 
     public Managers() {
-        torrentManager = new TorrentManager();
         siteManager = new SiteManager();
     }
 
     public void tick() {
         // TODO: 2016-02-15
 
+        ArrayList<String> imdbIds;
+        ArrayList<Torrent> torrents, matchedTorrents;
+
         //1 get active shows from imdb once every day and post it to the database
         //2 search thePirateBay and other sites after those torrents
-        //3 if it's a show we are looking for, add the torrent to the database.
-
-
-        // TODO: 2016-02-18
-        /*
-        isPageValid() in ImdbSeries is not working.
-        Returning true for none active show also.
-         */
+        //3 compare ids and torrents.
+        //4 if it's a show we are looking for, add the torrent to the database.
 
         //1
-        ArrayList<String> list = siteManager.getImdbIds(0, 50);
-
-
+        imdbIds = siteManager.getImdbIds(0, 50);
         //2
-        ArrayList<Torrent> torrents = siteManager.getRecentTorrentPages(20);
+        torrents = siteManager.getRecentTorrentPages(20);
 
-        int matches = 0;
+        //3
+        matchedTorrents = pairTorrentAndSeries(torrents, imdbIds);
 
+        System.out.println(String.format("the list was %d, found %d torrents, matches %d ", imdbIds.size(), torrents.size(), matchedTorrents.size()));
+        System.out.println(Common.toJson(matchedTorrents));
+
+        //4
+
+
+    }
+
+    private ArrayList<Torrent> pairTorrentAndSeries(ArrayList<Torrent> torrents, ArrayList<String> imdbIds) {
         ArrayList<Torrent> matchedTorrents = new ArrayList<>();
-        for (String s : list) {
+        for (String s : imdbIds) {
 
             ImdbSeries imdbSeries = siteManager.getSeries(s);
-            if (!imdbSeries.isPageValid()){
+            if (!imdbSeries.isPageValid()) {
                 System.out.println("not valid");
-                continue;
-            }
-
-            if(imdbSeries.getTitle() == null) {
-                System.out.println("series is null");
-                System.out.println(imdbSeries.getId());
                 continue;
             }
 
             for (Torrent torrent : torrents) {
 
-                if(!torrent.isSeries()){
+                if (!torrent.isSeries()) {
                     continue;
                 }
 
 
                 if (Common.isTorrentMatch(imdbSeries, torrent)) {
-                   // System.out.println(Common.toJson(torrent));
                     torrent.setMatch(imdbSeries.getTitle());
                     matchedTorrents.add(torrent);
-                    matches++;
                 }
 
             }
 
         }
-
-        System.out.println(String.format("the list was %d, found %d torrents, matches %d ", list.size(), torrents.size(), matches));
-        System.out.println(Common.toJson(matchedTorrents));
-
+        return matchedTorrents;
     }
 }

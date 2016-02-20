bbf04fefd4ec7c1f2d4154bdea7a6b0ab3284cd3
Robin.Edbom@gmail.com <Robin.Edbom@gmail.com>
2016-02-20 21:23:45 +0100

    RestApi now supports POST request

--- src/main/java/com/webcrawler/connections/restapi/RestApi.java
+++ src/main/java/com/webcrawler/connections/restapi/RestApi.java
@@ -1,6 +1,8 @@
 package com.webcrawler.connections.restapi;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -43,6 +45,41 @@ public abstract class RestApi {
         in.close();
 
         return response.toString();
+
+    }
+
+    protected void sendPost(String method,String urlParameters) throws Exception {
+
+        URL obj = new URL(getWholeUrl(method));
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
 
     }
 
